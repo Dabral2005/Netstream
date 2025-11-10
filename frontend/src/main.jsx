2823458a380d8669260a0456164f135d4e9f8851
Dabral <dabralmohit78@gmail.com>
2025-11-10 14:22:20 +0530

    Fix: Added BrowserRouter and corrected OAuth setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,15 +7,10 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId='769188610078-09ploddjd8tcke0l0jojkt9hsuruvtn5.apps.googleusercontent.com'>
-      
+    <GoogleOAuthProvider clientId="769188610078-09ploddjd8tcke0l0jojkt9hsuruvtn5.apps.googleusercontent.com">
+      <BrowserRouter>
         <App />
-     
+      </BrowserRouter>
     </GoogleOAuthProvider>
   </React.StrictMode>,
 )
-
-
-
-
-
