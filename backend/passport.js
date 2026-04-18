6965ac51745a0c793ab4fde78732a2d0cb5f9f72
Dabral <dabralmohit78@gmail.com>
2026-04-19 01:02:56 +0530

    backend: setup server structure and move dependencies to backend

--- backend/passport.js
+++ backend/passport.js
@@ -1,46 +1,17 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/user');
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    try {
-      let existingUser = await User.findOne({ googleId: profile.id });
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "/api/auth/google/callback",
+  scope: ['profile', 'email']
+}, async (accessToken, refreshToken, profile, done) => {
+  // Find or create user in MongoDB
+  const user = await User.findOne({ googleId: profile.id }) ||
+               await User.create({ googleId: profile.id, name: profile.displayName, email: profile.emails[0].value });
+  done(null, user);
+}));
 
-      if (existingUser) {
-        return done(null, existingUser);
-      }
-
-      const newUser = new User({
-        googleId: profile.id,
-        username: profile.displayName,
-        email: profile.emails[0].value
-      });
-
-      await newUser.save();
-      done(null, newUser);
-    } catch (err) {
-      done(err, null);
-    }
-  }
-));
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (err) {
-    done(err, null);
-  }
-});
-
-
-
+passport.serializeUser((user, cb) => cb(null, user.id));
+passport.deserializeUser((id, cb) => User.findById(id, cb));
